955e33a595c19cec4f96e1e8ad9979bb42ed9bca
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2025-08-12 13:30:31 +0530

    8th

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+import eel
 
 
 def speak(text):
@@ -8,24 +9,40 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening...')
+        eel.DisplayMessage('Listening...')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, timeout=10, phrase_time_limit=6)
 
     try:
         print('Recognizing...')
+        eel.DisplayMessage('Recognizing...')
         query = r.recognize_google(audio, language='en')
         print(f'User said: {query}')
+        # speak(query)
+        eel.DisplayMessage(query)
+        eel.ShowHood()
 
     except Exception as e:
         return ""
     return query.lower()
 
-text = takeCommand()
+# text = takeCommand()
 
-speak(text)
+# speak(text)
+@eel.expose
+def allCommands():
+    query = takeCommand()
+    print(query)
+
+    if 'open' in query:
+        from engine.features import openCommand
+        openCommand(query)
+    else:
+        print('Not Run')
 
--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,8 @@
 from playsound import playsound
 import eel
+from engine.command import speak
+from engine.config import ASSISTANT_NAME
+import os
 
 # sound function for playing sound
 
@@ -13,3 +16,16 @@ def playAssistantSound():
 def playClickSound():
     music_dir="www\\assets\\audio\\click_sound.mp3"
     playsound(music_dir)
+
+
+def openCommand(query):
+    query = query.replace(ASSISTANT_NAME, "")
+    query = query.replace("open", "")
+    query.lower()
+
+    if query != "":
+        speak("Opening "+ query)
+        os.system('start ' + query)
+
+    else:
+        speak(f"{query} not found")
